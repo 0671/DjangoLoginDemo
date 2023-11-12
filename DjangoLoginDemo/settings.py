d7cd16735b7b01393101d1c1958e2bac7bf0121f
0671 <h.vi@qq.com>
2023-11-13 04:36:51 +0800

    Update settings.py

--- DjangoLoginDemo/settings.py
+++ DjangoLoginDemo/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'yrwl$7suxmu&menainuq!f_z=&xh2ay=m542b7&s3edfzoyf#n'
+print(f'SECRET_KEY = "{SECRET_KEY}"')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
